278e60bd2202e91eb3426f1eb371e433570be16a
emretekinalp <e.tekinalp@icloud.com>
2017-10-29 14:02:03 +0100

    implemented additional code to test coverage

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ def test_this_func():
     print('This is running and expecting a raise Exception')
     # raise Exception('haha this fails')
 
+var = 5
+for i in range(var):
+    print i
+
 test_this_func()
