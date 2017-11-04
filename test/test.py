4469be3eaac837fc03aba8c3bddf39ce2504330c
emretekinalp <e.tekinalp@icloud.com>
2017-11-04 10:00:04 -0700

    renamed function

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
-def test_this_func(mult):
+def this_func(mult):
     a = 1
     b = 1
     return (a + b) * mult
 
 
-print('The result is {0} and'.format(test_this_func(20)))
+print('The result is {0} and'.format(this_func(20)))
