bb803bb67ce3c2b2161f9adfb306c2c37aee0c2d
emretekinalp <e.tekinalp@icloud.com>
2017-11-03 21:44:55 -0700

    removed unused lines

--- test.py
+++ test.py
@@ -1,41 +1,22 @@
 def test_this_func():
     print('This is running and expecting a raise Exception')
-    # raise Exception('haha this fails')
+    a = 1
+    b = 1
+    if (a + b) > 2:
+        raise Exception('haha this fails')
 
 var = 5
 for i in range(var):
     print i
 
-
 def not_call():
-    he = 'this code will not be called'
     return 'whuat?'
-
-
-
 
 def OUCHhah():
     a = 'bad naming here...jeeez'
     b = 'aaaaa'
-    c= 121221
-    de='adad' + 'adasddsds'
-    f =2222
-    worse=121312243
-    itsnotgettingBETTER=153423
-    oh=123123554545
-    h=4545
-    t=65665
-    c=6565656
     return 124
-
-
-test_this_func()
-
 
 print 'whaaat?'
 
-
-
-print('yep babublaba')
-test_this_func()
 test_this_func()
