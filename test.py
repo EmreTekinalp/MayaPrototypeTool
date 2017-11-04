1fab52ef37bfec3445e585e45ab9caf84d90c830
emretekinalp <e.tekinalp@icloud.com>
2017-11-04 05:04:07 -0700

    added bad code coverage for testing purposes

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 def test_this_func(mult):
     a = 1
     b = 1
+    c = 123
+    d = 345
+    if not c:
+        raise Exception('Wrong')
     return (a + b) * mult
 
 print('The result is {0}'.format(test_this_func(20)))
