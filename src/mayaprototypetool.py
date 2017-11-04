0013e9ce01090517466669f95a83323b33f654cf
emretekinalp <e.tekinalp@icloud.com>
2017-11-04 08:45:20 -0700

    cleaned up code in bth py modules

--- src/mayaprototypetool.py
+++ src/mayaprototypetool.py
@@ -1,4 +1,2 @@
 def run_this():
     return 'THis is annother module'
-
-print(run_this())
--- test/test.py
+++ test/test.py
@@ -1,28 +1,10 @@
 from src import mayaprototypetool
 
-mayaprototypetool.run_this()
-
 
 def test_this_func(mult):
     a = 1
     b = 1
-    c = 123
-    d = 345
-    if not c:
-        raise Exception('Wrong')
     return (a + b) * mult
 
 
-print('The result is {0}'.format(test_this_func(20)))
-
-
-def hahah():
-    arg = 223
-    b= 223232
-    c =11
-
-
-print hahah()
-
-
-print(123e204040)
+print('The result is {0} and {1}'.format(test_this_func(20), mayaprototypetool.run_this()))
